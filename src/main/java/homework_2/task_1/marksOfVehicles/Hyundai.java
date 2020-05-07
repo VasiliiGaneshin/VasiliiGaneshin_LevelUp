f7de5bdc3c476743597c79181f039f305ea40622
VasiliyGaneshin <ganeshin.vasilii@gmail.com>
2020-05-07 15:17:22 +0300

    Homework 2

---
+++ src/main/java/homework_2/task_1/marksOfVehicles/Hyundai.java
@@ -0,0 +1,8 @@
+package homework_2.task_1.marksOfVehicles;
+
+public class Hyundai extends Vehicle {
+
+    public Hyundai(double fuelConsumption, String engineType, String size, int capacity) {
+        super(fuelConsumption, engineType, size, capacity);
+    }
+}
---
+++ src/main/java/homework_2/task_1/marksOfVehicles/Man.java
@@ -0,0 +1,8 @@
+package homework_2.task_1.marksOfVehicles;
+
+public class Man extends Vehicle {
+
+    public Man(double fuelConsumption, String engineType, String size, int capacity) {
+        super(fuelConsumption, engineType, size, capacity);
+    }
+}
